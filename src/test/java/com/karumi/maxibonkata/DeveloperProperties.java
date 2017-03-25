c66dd711a2c107a996ca66169b533a432df482c2
Xavier Rubio Jansana <xavier@toro-intl.com>
2017-03-25 12:11:02 +0100

    Unit test consumed maxibons is correct

--- src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -3,8 +3,10 @@ package com.karumi.maxibonkata;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnitQuickcheck.class)
@@ -15,5 +17,16 @@ public class DeveloperProperties {
         Developer developer = new Developer("Xavi", maxibons);
         System.out.println(developer.toString());
         assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
+    }
+
+    @Test
+    public void testConsumedMaxibonsIsCorrect() {
+        int maxibons = 3;
+        Developer developer = new Developer("Xavi", maxibons);
+        KarumiHQs karumiHQs = new KarumiHQs(new ConsoleChat());
+        int maxibonsLeftBefore = karumiHQs.getMaxibonsLeft();
+        karumiHQs.openFridge(developer);
+        int maxibonsLeftAfter = karumiHQs.getMaxibonsLeft();
+        assertEquals(maxibonsLeftAfter, maxibonsLeftBefore - maxibons);
     }
 }
